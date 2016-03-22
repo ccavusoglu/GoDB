338d92f127e57668d93fb9b8a0c0a4b4a6be4ba8
Ç. Çavuşoğlu <cavusoglucagatay@gmail.com>
2016-03-23 01:51:33 +0200

    initial
    initial commit

---
+++ app/src/main/java/com/godb/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.godb.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.example.godb.app.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
