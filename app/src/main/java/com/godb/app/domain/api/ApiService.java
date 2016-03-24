d9544c6c9c0a5440b040995b658e2ff6839604b1
Ç. Çavuşoğlu <cavusoglucagatay@gmail.com>
2016-03-24 17:18:58 +0200

    api service

---
+++ app/src/main/java/com/godb/app/domain/api/ApiService.java
@@ -0,0 +1,18 @@
+package com.godb.app.domain.api;
+
+import com.godb.app.presentation.models.Announcement;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+import java.util.List;
+
+/**
+ * Created by Q on 23.03.2016.
+ */
+public interface ApiService {
+    String SERVICE_ENDPOINT = "https://localhost/api";
+
+    @GET("/users/{path}")
+    Observable<List<Announcement>> getAnnouncements(@Path("announcements") String path);
+}
---
+++ app/src/main/java/com/godb/app/domain/api/ServiceFactory.java
@@ -0,0 +1,16 @@
+package com.godb.app.domain.api;
+
+import retrofit2.Retrofit;
+
+/**
+ * Created by Q on 23.03.2016.
+ */
+public class ServiceFactory {
+    public static <T> T createService(final Class<T> clazz, final String endPoint) {
+        final Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(endPoint)
+                .build();
+
+        return retrofit.create(clazz);
+    }
+}
